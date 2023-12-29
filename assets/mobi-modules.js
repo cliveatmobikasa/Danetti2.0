738c03eb5b5f0039ba6d08afb61a384883ed160c
cliveatmobikasa <clive@mobikasa.com>
2023-12-29 15:32:45 -0500

    update  collection page

--- assets/mobi-modules.js
+++ assets/mobi-modules.js
@@ -773,6 +773,688 @@ class ArticleCarousel extends Module {
     }
   }
 }
+
+class ScrollTo extends Module {
+  static methods = ['smoothScroll'];
+  static targets = ['trigger'];
+
+  initialize (el, context) {
+    super.initialize(el, context);
+  }
+
+  setupListeners () {
+    super.setupListeners();
+    this.triggerEls.forEach(trigger => {
+      this.addListeners(trigger, 'click', () => {
+        this.smoothScroll(trigger);
+      })
+    })
+  }
+
+  smoothScroll (el) {
+    window.scrollTo({
+      top: getOffsetTop(getElement(`[data-scroll-to-element="${el.dataset.scrollToTarget}"]`)),
+      left: 0,
+      behavior: 'smooth',
+    })
+  }
+}
+
+class UnTruncate extends Module {
+  static targets = ['triggerOpen', 'triggerClose', 'fullContent', 'content'];
+  static methods = ['unTruncate', 'truncate'];
+
+  initialize (el, context) {
+    super.initialize(el, context);
+  }
+
+  setupListeners () {
+    this.addListeners(this.triggerOpenEl, 'click', this.unTruncate.bind(this));
+    this.addListeners(this.triggerCloseEl, 'click', this.truncate.bind(this));
+  }
+
+  unTruncate (e) {
+    this.fullContentEl.style.display = 'block'
+    this.contentEl.style.display = 'none'
+  }
+
+  truncate (e) {
+    this.fullContentEl.style.display = 'none'
+    this.contentEl.style.display = 'block'
+  }
+}
+class Collection extends Module {
+  static methods = [
+    'onScroll',
+    'onHistoryChange',
+    'onFacetRemovalClick',
+    'onFacetSummaryClick',
+    'onFacetBackClick',
+    'onKeyUp',
+    'onSubmit',
+    'renderPage',
+    'updateURLHash',
+    'fromCache',
+    'fromFetch',
+    'render',
+    'renderFilters',
+    'renderActiveFacets',
+    'renderAdditionalElements',
+    'renderProductGridContainer',
+    'setupObservers',
+    'renderProductGridPagination',
+    'renderProductCount',
+    'renderSelectedFacetCount',
+  ]
+
+  static targets = [
+    'form',
+    'facets',
+    'facet',
+    'facetRemove',
+    'facetsSelected',
+    'facetSummary',
+    'facetBack',
+    'activeFacets',
+    'productGrid',
+    'productCount',
+    'header',
+    'sortBy',
+  ]
+
+  initialize (el, context) {
+    super.initialize(el, context);
+    this.sections = [
+      {
+        id: `shopify-section-${this.el.dataset.id}`,
+        section: this.el.dataset.id,
+        selector: '.shopify-section',
+      },
+    ]
+
+    this.cssSelectors = {
+      form: '[data-el="collection.form"]',
+      facetsContainer: '[data-el="collection.facets"]',
+      facet: '[data-el="collection.facet"]',
+      facetRemove: '[data-el="collection.facetRemove"]',
+      facetsSelected: '[data-el="collection.facetsSelected"]',
+      facetSummary: '[data-el="collection.facetSummary"]',
+      facetBack: '[data-el="collection.facetBack"]',
+      activeFacetsContainer: '[data-el="collection.activeFacets"]',
+      productCount: '[data-el="collection.productCount"]',
+      productGrid: '[data-el="collection.productGrid"]',
+      pagination: '[data-el="collection.pagination"]',
+      header: '[data-el="collection.header"]',
+    }
+
+    this.filterData = []
+
+    this.searchParams = {
+      // eslint-disable-next-line compat/compat
+      previous: new URLSearchParams(window.location.search),
+      // eslint-disable-next-line compat/compat
+      current: new URLSearchParams(window.location.search),
+      // eslint-disable-next-line compat/compat
+      initial: new URLSearchParams(window.location.search),
+    }
+  }
+
+  setupListeners () {
+    super.setupListeners();
+    this.addListeners(this.el, 'click', this.onFacetRemovalClick.bind(this))
+    this.addListeners(this.el, 'click', this.onFacetSummaryClick.bind(this))
+    this.addListeners(this.el, 'click', this.onFacetBackClick.bind(this))
+
+    this.formEls.forEach(form => {
+      this.addListeners(form, 'input', this.onSubmit.bind(this))
+      this.addListeners(form, 'submit', this.onSubmit.bind(this))
+    })
+
+    this.addListeners(window, 'popstate', this.onHistoryChange.bind(this))
+
+    this.facetsEls.forEach(facetsContainer => {
+      this.addListeners(facetsContainer, 'keyup', this.onKeyUp.bind(this))
+    })
+
+    this.addListeners(this.sortByEl, 'change', this.onSubmit.bind(this))
+
+    this.addListeners(window, 'scroll', this.onScroll)
+
+    this.addListeners(document, 'click', this.onPaginationClick.bind(this))
+  }
+
+  onScroll () {
+    const filterHeader = getElement('.clc-Filters_Header')
+    if (!filterHeader) {
+      return
+    }
+    if (window.scrollY > filterHeader.offsetTop + filterHeader.offsetHeight) {
+      filterHeader.classList.add('clc-Filters_Header-sticky')
+    } else {
+      filterHeader.classList.remove('clc-Filters_Header-sticky')
+    }
+  }
+
+  onHistoryChange (e) {
+    const searchParams = e.state ? e.state.searchParams : this.searchParams.initial
+    if (searchParams.toString() === this.searchParams.previous.toString()) {
+      return
+    }
+
+    this.renderPage({ searchParams, updateURLHash: false })
+  }
+
+  onFacetRemovalClick (e) {
+    const facet = e.target.closest(this.cssSelectors.facetRemove)
+
+    if (!facet) {
+      return true
+    }
+
+    e.preventDefault()
+    e.stopPropagation()
+
+    const href = facet.querySelector('a').href
+
+    const searchParams = href.includes('?') ? href.slice(href.indexOf('?') + 1) : ''
+
+    this.renderPage({ searchParams })
+  }
+
+  onFacetSummaryClick (e) {
+    const facetSummary = e.target.closest(this.cssSelectors.facetSummary)
+
+    if (!facetSummary) {
+      return true
+    }
+
+    const facet = e.target.closest(`${this.cssSelectors.facet}`)
+
+    if (!facet) return
+
+    const isOpen = facet.getAttribute('aria-expanded') === 'true'
+    facet.setAttribute('aria-expanded', !isOpen)
+    facetSummary.setAttribute('aria-expanded', !isOpen)
+  }
+
+  onFacetBackClick (e) {
+    const back = e.target.closest(this.cssSelectors.facetBack)
+
+    if (!back) {
+      return true
+    }
+
+    e.preventDefault()
+
+    const facet = e.target.closest(this.cssSelectors.facet)
+    const facetSummary = facet.querySelector(`${this.cssSelectors.facetSummary}`)
+
+    const isOpen = facet.getAttribute('aria-expanded') === 'true'
+    facet.setAttribute('aria-expanded', !isOpen)
+    facetSummary.setAttribute('aria-expanded', !isOpen)
+  }
+
+  onKeyUp (e) {
+    if (e.code.toUpperCase() !== 'ESCAPE') return
+
+    const openFacet = e.target.closest(`${this.cssSelectors.facet}[open]`)
+    if (!openFacet) return
+
+    const summary = getElement(`${this.cssSelectors.facetSummary}`, {
+      context: openFacet,
+    })
+    openFacet.removeAttribute('open')
+    summary.setAttribute('aria-expanded', 'false')
+    summary.focus()
+  }
+
+  onPaginationClick (e) {
+    if (!e.target.closest('.clc-Detail_Pagination')) return
+
+    e.preventDefault()
+
+    const anchor = e.target.closest('a')
+    if (!anchor || anchor.getAttribute('href') === '' || anchor.disabled) return
+
+    const href = anchor.getAttribute('href')
+    // eslint-disable-next-line compat/compat
+    const paginationSearchParams = new URLSearchParams(href.split('?')[1])
+    const wantedPage = paginationSearchParams.get('page') ?? '1'
+    // eslint-disable-next-line compat/compat
+    const currentSearchParams = new URLSearchParams(this.searchParams.current.toString())
+    currentSearchParams.set('page', wantedPage)
+
+    const headerTop = getElement(`${this.cssSelectors.header}`).offsetTop - 150
+
+    window.scrollTo({
+      top: headerTop,
+      behavior: 'smooth',
+    })
+
+    this.renderPage({ searchParams: currentSearchParams.toString() })
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+
+    if (e.target.className === 'prc-Range_Input') {
+      return false
+    }
+
+    const formData = new FormData(e.target.closest('form'))
+    // eslint-disable-next-line compat/compat
+    const searchParams = new URLSearchParams(formData).toString()
+
+    this.renderPage({ searchParams, e })
+  }
+
+  renderPage ({ searchParams, e, updateURLHash = true } = {}) {
+    this.searchParams.previous = this.searchParams.current
+    // eslint-disable-next-line compat/compat
+    this.searchParams.current = new URLSearchParams(searchParams)
+
+    this.loading = true
+
+    this.sections.forEach(section => {
+      const url = `${window.location.pathname}?section_id=${
+        section.section
+      }&${this.searchParams.current.toString()}`
+
+      const filterDataUrl = element => element.url === url
+
+      this.filterData.some(element => filterDataUrl(element))
+        ? this.fromCache(filterDataUrl, e)
+        : this.fromFetch(section.section, url, e)
+    })
+
+    if (updateURLHash) this.updateURLHash(this.searchParams.current)
+  }
+
+  updateURLHash (searchParams) {
+    history.pushState(
+      { searchParams: searchParams.toString() },
+      '',
+      `${window.location.pathname}${searchParams && '?'.concat(searchParams.toString())}`,
+    )
+  }
+
+  get loading () {
+    return this._loading
+  }
+
+  set loading (val) {
+    this._loading = val
+
+    getElement(this.cssSelectors.productGrid).setAttribute('aria-busy', val.toString())
+  }
+
+  fromCache (filterDataUrl, e) {
+    const html = this.filterData.find(element => filterDataUrl(element)).html
+
+    this.render(html, e)
+  }
+
+  async fromFetch (section, url, e) {
+    const html = await SectionRenderer.fetchSingle(section, url)
+
+    this.filterData.push({ html, url })
+
+    this.render(html, e)
+  }
+
+  render (html, e) {
+    this.renderFilters(html, e)
+    this.renderProductGridContainer(html)
+    this.renderProductGridPagination(html)
+    this.renderProductCount(html)
+
+    this.loading = false
+  }
+
+  renderFilters (html, e) {
+    const parsedHTML = new DOMParser().parseFromString(html, 'text/html')
+    const facetDetailElements = getElements(this.cssSelectors.facet, {
+      context: parsedHTML,
+    })
+
+    facetDetailElements.forEach(element => {
+      const selector = this.cssSelectors.facet
+        .split(',')
+        .map(selector => `${selector}[data-index="${element.dataset.index}"]`)
+        .join(',')
+
+      const el = getElement(selector)
+
+      el.innerHTML = element.innerHTML
+    })
+
+    this.renderActiveFacets(parsedHTML)
+    this.renderAdditionalElements(parsedHTML)
+  }
+
+  /**
+   * @param {Document} parsedHTML
+   *
+   * @returns {void}
+   */
+  renderActiveFacets (parsedHTML) {
+    const activeFacetElements = getElements(this.cssSelectors.activeFacetsContainer, {
+      context: parsedHTML,
+    })
+    if (!activeFacetElements || !activeFacetElements.length) return
+
+    this.activeFacetsEls.forEach(
+      (activeFacetsContainer, index) =>
+        (activeFacetsContainer.innerHTML = activeFacetElements[index].innerHTML),
+    )
+  }
+
+  /**
+   * @param {Document} parsedHTML
+   *
+   * @returns {void}
+   */
+  renderAdditionalElements (parsedHTML) {}
+
+  /**
+   * @param {String} html
+   *
+   * @returns {void}
+   */
+  renderProductGridContainer (html) {
+    const parsedHTML = new DOMParser().parseFromString(html, 'text/html')
+
+    getElement(this.cssSelectors.productGrid).innerHTML = getElement(
+      this.cssSelectors.productGrid,
+      {
+        context: parsedHTML,
+      },
+    ).innerHTML
+
+    this.setupObservers()
+  }
+
+  setupObservers () {
+    this.onTriggerEnter = this.onTriggerEnter.bind(this)
+
+    const animationSections = getElements('[ data-animate-section]')
+
+    const offsetAmount = window.MOBIKASA.mediaBreakpoints.isXsViewport ? 100 : 200
+
+    this.observer = new IntersectionObserver(this.onTriggerEnter, {
+      rootMargin: `-${offsetAmount}px`,
+    })
+    animationSections.forEach(animationSection => {
+      this.observer.observe(animationSection)
+    })
+  }
+
+  onTriggerEnter (entries) {
+    const sectionsEntered = entries.filter(entry => entry.isIntersecting)
+
+    sectionsEntered.forEach(entry => {
+      const contentEls = getElements('[data-animate-el]', { context: entry.target })
+      const imageEls = getElements('[data-animate-image]', { context: entry.target })
+
+      this.observer.unobserve(entry.target)
+
+      if (imageEls) {
+        const imageTimeline = gsap.timeline({ delay: 0.2 })
+        imageTimeline.to(imageEls, 1.5, {
+          opacity: 1,
+          ease: 'expo.out',
+        })
+      }
+
+      if (contentEls) {
+        const contentTimeline = gsap.timeline({ delay: 0.2 })
+        contentTimeline.staggerTo(
+          contentEls,
+          1,
+          {
+            autoAlpha: 1,
+            y: '0%',
+            ease: 'expo.out',
+          },
+          0.2,
+        )
+      }
+    })
+  }
+
+  /**
+   * @param {String} html
+   *
+   * @returns {void}
+   */
+  renderProductGridPagination (html) {
+    const parsedHTML = new DOMParser().parseFromString(html, 'text/html')
+
+    const pagination = getElement(this.cssSelectors.pagination)
+    const newPagination = getElement(this.cssSelectors.pagination, {
+      context: parsedHTML,
+    })
+
+    if (pagination && !newPagination) {
+      pagination.remove()
+      return
+    }
+
+    if (pagination) {
+      pagination.innerHTML = newPagination.innerHTML
+    } else if (newPagination) {
+      this.productGridEl.parentNode.insertBefore(newPagination, this.productGridEl.nextSibling)
+    }
+
+    const loadMore = getElement('.clc-List', {
+      context: parsedHTML,
+    })
+
+    Object.keys(loadMore.dataset).forEach(key => {
+      getElement('.clc-List').dataset[key] = loadMore.dataset[key]
+    })
+
+    window.MOBIKASA.application.reinitialiseModules(this.el)
+  }
+
+  /**
+   * @param {String} html
+   *
+   * @returns {void}
+   */
+  renderProductCount (html) {
+    const parsedHTML = new DOMParser().parseFromString(html, 'text/html')
+    const count = getElement(this.cssSelectors.productCount, {
+      context: parsedHTML,
+    })
+
+    if (!count) return
+
+    this.countElements.forEach(element => (element.innerHTML = count.innerHTML))
+  }
+
+  /**
+   * @param {HTMLElement} source
+   * @param {HTMLElement} target
+   *
+   * @returns {void}
+   */
+  renderSelectedFacetCount (source, target) {
+    const sourceElement = getElement(this.cssSelectors.facetsSelected, {
+      context: source,
+    })
+    const targetElement = getElement(this.cssSelectors.facetsSelected, {
+      context: target,
+    })
+
+    if (sourceElement && targetElement) {
+      targetElement.outerHTML = sourceElement.outerHTML
+    }
+  }
+
+  /**
+   * @returns {Element[]}
+   */
+  get countElements () {
+    return getElements('[data-product-grid-count]')
+  }
+}
+
+class CollectionListWithPromotions extends Module {
+  static methods = ['onWindowResize']
+  static targets = ['item', 'promotion', 'promotionImage']
+
+  initialize (el, context) {
+    super.initialize(el, context);
+  }
+
+  setupListeners () {
+    super.setupListeners();
+    this.promotionEls.forEach(el => this.sizePromotion(el));
+    const throttled = debounce(this.onWindowResize.bind(this), 250);
+    this.addListeners(window, 'resize', throttled);
+  }
+  onWindowResize () {
+    this.promotionEls.forEach(promotion => {
+      const promotionImage = getElement(this.selectors.promotionImage, { context: promotion })
+      promotionImage.style.height = ''
+
+      this.sizePromotion(promotion)
+    })
+  }
+
+  sizePromotion (promotion) {
+    if (window.MOBIKASA.mediaBreakpoints.isXsViewport && promotion.dataset['promotionWidth'] != 1) return
+
+    const relevantItems = this.getLargestItemsOnEachRow(this.getRelevantItems(promotion));
+    const firstOffsetTop = relevantItems[0].offsetTop;
+    const lastItem = relevantItems.pop();
+    const toTopOfSecondRow = lastItem.offsetTop - firstOffsetTop
+    const heightOfImage = getElement('.prd-Card_ImageContainer', {
+      context: lastItem.item,
+    }).offsetHeight
+    const promotionImage = getElement(this.selectors.promotionImage, { context: promotion })
+
+    promotionImage.style.height = `${toTopOfSecondRow + heightOfImage}px`
+  }
+
+  getRelevantItems (promotion) {
+    const promotionOffsetTop = getOffsetTop(promotion)
+    const promotionImageContainer = getElement(this.selectors.promotionImage, {
+      context: promotion,
+    })
+    const endOfImage = promotionOffsetTop + promotionImageContainer.offsetHeight
+
+    return this.itemEls.filter(item => {
+      const itemOffsetTop = getOffsetTop(item)
+
+      return promotionOffsetTop <= itemOffsetTop && itemOffsetTop <= endOfImage
+    })
+  }
+
+  getLargestItemsOnEachRow (items) {
+    return items
+      .map(item => ({ offsetTop: getOffsetTop(item), height: item.offsetHeight, item }))
+      .reduce((acc, item) => {
+        const currentlyHasThisOffset = acc.some(i => i.offsetTop === item.offsetTop)
+        if (currentlyHasThisOffset) {
+          const currentIndex = acc.findIndex(i => i.offsetTop === item.offsetTop);
+
+          if (item.height > acc[currentIndex]) {
+            acc[currentIndex] = item;
+          }
+        } else {
+          acc.push(item);
+        }
+
+        return acc
+      }, [])
+  }
+}
+
+
+class PriceRange extends HTMLElement {
+  static get requiredElements () {
+    return ['min', 'max']
+  }
+
+  constructor () {
+    super()
+
+    this.setupListeners()
+    this.setMinAndMaxValues()
+    this.timeout = null
+  }
+
+  setupListeners () {
+    this.onRangeChange = this.onRangeChange.bind(this)
+
+    getElements('input', { context: this }).forEach(el =>
+      el.addEventListener('change', this.onRangeChange),
+    )
+  }
+
+  /**
+   * @param {InputEvent} e
+   *
+   * @returns {void}
+   */
+  onRangeChange (e) {
+    this.adjustToValidValues(e.currentTarget)
+    this.setMinAndMaxValues()
+
+    clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      this.closest('form').dispatchEvent(new Event('submit'))
+    }, 500)
+  }
+
+  setMinAndMaxValues () {
+    if (this.maxInput.value) {
+      this.minInput.setAttribute('max', this.maxInput.value)
+    }
+    if (this.minInput.value) {
+      this.maxInput.setAttribute('min', this.minInput.value)
+    }
+    if (this.minInput.value === '') this.maxInput.setAttribute('min', '0')
+    if (this.maxInput.value === '') {
+      this.minInput.setAttribute('max', this.maxInput.getAttribute('max'))
+    }
+  }
+
+  /**
+   * @param {HTMLInputElement} input
+   *
+   * @returns {void}
+   */
+  adjustToValidValues (input) {
+    const value = Number(input.value)
+    const min = Number(input.getAttribute('min'))
+    const max = Number(input.getAttribute('max'))
+
+    if (value < min) input.value = min.toString()
+    if (value > max) input.value = max.toString()
+  }
+
+  /**
+   * @returns {HTMLInputElement}
+   */
+  get maxInput () {
+    return getElement('[data-price-range-el="max"]', { context: this })
+  }
+
+  /**
+   * @returns {HTMLInputElement}
+   */
+  get minInput () {
+    return getElement('[data-price-range-el="min"]', { context: this })
+  }
+}
+
+if (!customElements.get('price-range')) {
+  customElements.define('price-range', PriceRange)
+}
 window.MOBIKASA.modules = {
   Application,
   Header,
@@ -782,5 +1464,9 @@ window.MOBIKASA.modules = {
   FooterAccordions,
   ProductCarousel,
   CtaCarousel,
-  ArticleCarousel
+  ArticleCarousel,
+  ScrollTo,
+  UnTruncate,
+  Collection,
+  CollectionListWithPromotions
 };
--- assets/mobi-utils.js
+++ assets/mobi-utils.js
@@ -190,3 +190,197 @@ const createObject = (className, el, application) => {
     return null;
   }
 };
+
+const getOffsetTop = (el, parent = document.body, offsetTop = 0) => {
+  if (!isNaN(el.offsetTop)) {
+    offsetTop += el.offsetTop;
+  }
+  return el.offsetParent === parent ? offsetTop : getOffsetTop(el.offsetParent, parent, offsetTop);
+}
+
+class SectionRendering {
+  /**
+   * Request a section, or sections, from the section rendering API
+   *
+   * @param {string|string[]} sections - The names of the sections we want to fetch
+   * @param {string} [sectionsUrl] - The context in which we want the sections to be
+   *   rendered in
+   *
+   * @returns {Promise<Object>}
+   */
+  async fetch (sections, sectionsUrl = this.sectionsUrl) {
+    if (typeof sections === 'string') {
+      sections = [sections]
+    }
+
+    const [path, querystring = ''] = sectionsUrl.split('?')
+
+    // eslint-disable-next-line compat/compat
+    const params = new URLSearchParams(querystring)
+    params.append('sections', sections.join(','))
+
+    const section = await fetch(`${path}?${params.toString()}`)
+
+    return section.json()
+  }
+
+  /**
+   * Request a single section from the section rendering API
+   * https://shopify.dev/api/section-rendering#request-a-single-section
+   *
+   * @param {string} section - The names of the section we want to fetch
+   * @param {string} [sectionsUrl] - The context in which we want the sections to be
+   *   rendered in
+   *
+   * @returns {Promise<string>}
+   */
+  async fetchSingle (section, sectionsUrl = window.location.pathname) {
+    const [path, querystring = ''] = sectionsUrl.split('?')
+
+    // eslint-disable-next-line compat/compat
+    const params = new URLSearchParams(querystring)
+    params.set('section_id', section)
+
+    const response = await fetch(`${path}?${params.toString()}`)
+
+    return response.text()
+  }
+
+  /**
+   * Render a section returned from the section rendering API
+   *
+   * @param {Section} section - The makeup of the section we want to render
+   *
+   * @returns {void}
+   */
+  render (section) {
+    if ('dontRender' in section && section.dontRender) return
+
+    this.validate(section)
+
+    const elementToReplace =
+      document.getElementById(section.id).querySelector(section.selector) ||
+      document.getElementById(section.id)
+
+    const newHTML = this.getInnerHtml(section.html, section.selector)
+    const oldHTML = elementToReplace.innerHTML
+
+    elementToReplace.innerHTML =
+      typeof section.convertor === 'function' ? section.convertor(oldHTML, newHTML) : newHTML
+  }
+
+  /**
+   * Render multiple sections returned from the section rendering API
+   *
+   * @param {Section[]} sections - The sections we want to render
+   *
+   * @returns {void}
+   */
+  renderMultiple (sections) {
+    sections.forEach(section => this.render(section))
+  }
+
+  /**
+   * Grab the HTML that is inside the section otherwise we end up with section inception
+   *
+   * @param {string} html - The section HTML returned from the section rendering API
+   * @param {string} selector - The CSS selector which contains the sections content
+   *
+   * @returns {string}
+   */
+  getInnerHtml (html, selector = '.shopify-section') {
+    return this.convertToDOM(html).querySelector(selector).innerHTML
+  }
+
+  /**
+   * Convert a HTML string into a parseable DOM
+   *
+   * @param {string} html
+   *
+   * @returns {Document}
+   */
+  convertToDOM (html) {
+    return new DOMParser().parseFromString(html, 'text/html')
+  }
+
+  /**
+   * Render a section returned from the section rendering API
+   *
+   * @param {Section} section - The makeup of the section we want to render
+   *
+   * @returns {boolean}
+   */
+  validate (section) {
+    const isValid = this.expectedKeys.every(key => key in section)
+
+    if (!isValid) {
+      throw new Error(
+        `section is missing these keys: "${this.expectedKeys
+          .filter(k => !(k in section))
+          .join(', ')}"`,
+      )
+    }
+
+    return isValid
+  }
+
+  get expectedKeys () {
+    return ['id', 'selector', 'html']
+  }
+
+  /**
+   * @returns {Section[]}
+   */
+  get pushCartSections () {
+    return [
+      {
+        id: 'push-cart-items',
+        section: 'push-cart-items',
+        selector: '.shopify-section',
+      },
+      {
+        id: 'push-cart-footer',
+        section: 'push-cart-footer',
+        selector: '.shopify-section',
+      },
+      {
+        id: 'push-cart-count',
+        section: 'push-cart-count',
+        selector: '.shopify-section',
+        dontRender: true,
+      },
+    ]
+  }
+
+  /**
+   * @returns {Section[]}
+   */
+  get cartSections () {
+    return [
+      {
+        id: 'push-cart-count',
+        section: 'push-cart-count',
+        selector: '.shopify-section',
+        dontRender: true,
+      },
+      {
+        id: 'template-cart-items',
+        section: document.getElementById('template-cart-items').dataset.id,
+        selector: '.js-Contents',
+      },
+      {
+        id: 'template-cart-footer',
+        section: document.getElementById('template-cart-footer').dataset.id,
+        selector: '.js-Contents',
+      },
+    ]
+  }
+
+  /**
+   * @returns {string} The current url pathname and query parameters
+   */
+  get sectionsUrl () {
+    return `${window.location.pathname}${window.location.search}`
+  }
+}
+const SectionRenderer = new SectionRendering();
